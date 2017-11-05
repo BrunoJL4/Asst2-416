7def44e747e5979d95d0cf0c257cb76c1d17affa
Alex <Alex>
2017-11-04 21:43:25 -0400

    myallocate complete for phase A and ready for review

--- mymalloc.c
+++ mymalloc.c
@@ -21,6 +21,9 @@ uint manager_active;
 /* The global array containing the memory we are "allocating" */
 static char myBlock[TOTALMEM];
 
+/* This is the page table */
+char * pageTable[MAX_NUM_THREADS + 2]; //+2 so that our pthread library "manager" index is not out-of-bounds
+
 /* End global variable declarations. */
 
 /* malloc & free function implementations */
@@ -35,8 +38,7 @@ void* myallocate(int bytes, char * file, int line, int req){
 		   1. Enough space for pnodes we allocate plus buffer.
 		   This would be about 2 * MAX_NUM_THREADS * sizeof(pnode)
 		   2. Enough space for all of the TCB's. This  would be
-		   MAX_NUM_THREADS*sizeof(tcb).
-		   3. Space for MLPQ and tcbList. This is the size of a
+		   MAX_NUM_THREADS*sizeof(tcb) size of a
 		   pointer, times 2.
 		   4. Space for all of the stacks we allocate. This will
 		   be, in the worst case, MAX_NUM_THREADS * MEM. MEM
@@ -47,8 +49,8 @@ void* myallocate(int bytes, char * file, int line, int req){
 		// say kernel is not free, amount of freed space will be: 
 
 		//Kernel memory space is defined by this metadata
-		int kernalSize = (sizeof(Metadata) + (2 * MAX_NUM_THREAD * size(pnode)) + (MAX_NUM_THREAD * sizeof(tcb)) + (sizeof(pnode *) + sizeof(tcbList *)) + (MAX_NUM_THREAD * MEM) + (MAX_NUM_THREAD * sizeof(int *)));
-		Metadata data = {"BLOCK_USED", kernalSize};
+		int kernalSize = (sizeof(Metadata) + (2 * MAX_NUM_THREADS * size(pnode)) + (MAX_NUM_THREADS * sizeof(tcb)) + (sizeof(pnode *) + sizeof(tcbList *)) + (MAX_NUM_THREADS * MEM) + (MAX_NUM_THREADS * sizeof(int *)));
+		Metadata data = { BLOCK_USED, kernalSize};
 		//Throw this meta data to the front of our memory block
 		*(Metadata *)myBlock = data;
 		
@@ -56,39 +58,84 @@ void* myallocate(int bytes, char * file, int line, int req){
 		//This is the remaining mem modulo size of the page
 		//Remaining mem is MEM - kernel space + metadata
 		int remainingMem = (MEM - kernalSize) % (PAGESIZE);
-		myBlock->size += remainingMem;
+		(Metadata *)myBlock->size += remainingMem;
+		pageTable[MAX_NUM_THREADS + 1] = &myBlock;
 		
 		//Create metadata for each page
 		//Continue jumping by pagesize
 		char * ptr = &myBlock + myBlock->size();
 		while (ptr < &myBlock + TOTALMEM) {
-			data = {"BLOCK_FREE", PAGESIZE };
+			data = { BLOCK_FREE, PAGESIZE };
 			*(Metadata *)ptr = data;
-			ptr += ptr->size;
+			ptr += (Metadata *)ptr->size;
 		}
 		
 		//End of initial page and kernel setup	
 	}
 	
-	//if calling user thread does not have a page
-		//assign a page
-	//else retrieve the page associated with thread
-	
-	//start at initial page meta data
-	//continue to jump from different meta data to the next
-		//if block is free
-			//combine with any adjacent free blocks
-			//If block is big enough
-				//divide into requested size and return
-			//else continue
-	
-	if(req){ //LIB called 
-		//allocate block in sys side of mem 
-	}else{ //USR called
-		//check page table
-		//does respective page have room for malloc call?
+	//Check if the calling thread has a page
+	//If so, search for available space
+	if (pageTable[current_thread] != NULL) {
+		//This thread has a page, look for segment
+		char * ptr = pageTable[current_thread] + sizeof(Metadata);
+		while (ptr < pageTable[current_thread] + PAGESIZE) {
+			//If the segment is free, check if the next is also free, then check if it is enough space
+			if ((SegMetadata *)ptr->used == BLOCK_FREE) {
+				//If nextPtr is still in bounds, check if it is free
+				if (ptr + (SegMetadata *)ptr->size < pageTable[current_thread] + PAGESIZE) {
+					char * nextPtr = ptr + (SegMetadata *)ptr->size;
+					//If nextPtr is free, combine with ptr
+					if ((SegMetadata *)nextPtr->used == BLOCK_FREE) {
+						(SegMetadata *)ptr->size += (SegMetadata *)nextPtr->size + sizeof(SegMetadata); //Treat metadata for segments as if being outside the segment
+						//@all (mostly @Bruno): wipe the metadata of nextPtr
+					}
+				}
+				//If this segment has enough space, assign user requested space, set remaining to free, and return
+				if ((SegMetadata *)ptr->size <= bytes) {
+					SegMetadata segment = { BLOCK_USED, bytes };
+					//If all bytes were not used, create another free segment (+/-sizeof(SegMetadata) considering leakage)
+					if ((SegMetadata *)ptr->size < (bytes + sizeof(SegMetadata))) {
+						char * nextPtr = ptr + sizeof(SegMetadata) + (SegMetadata *)ptr->size;
+						SegMetadata nextSegment = { BLOCK_FREE, (SegMetadata *)ptr->size - (bytes + sizeof(SegMetadata)) };
+						*(SegMetadata *)nextPtr = nextSegment;
+						(SegMetadata *)ptr->size -= sizeof(SegMetadata) + (SegMetadata *)nextPtr;
+					}
+					return ptr + sizeof(SegMetadata);
+				}
+			}
+			ptr += (SegMetadata *)ptr->size;
+		}
+		//If we reached this point, no segments available, return NULL (Phase A)
+		return NULL;
+		
+	} else { //Assign a page, return NULL if none available (Phase A)
+		char * ptr = &myBlock + myBlock->size();
+		while (ptr < &myBlock + TOTALMEM) {
+			//If this page is not being used, claim it
+			if ((Metadata *)ptr->used == BLOCK_FREE) {
+				pageTable[current_thread] = ptr;
+				break;
+			}
+			ptr += PAGESIZE;
+		}
+		//If no page was found, return NULL (Phase A)
+		if (pageTable[current_thread] == NULL) {
+			return NULL;
+		}
+		
+		//We have assigned a page, give memory segment
+		SegMetadata segment = { BLOCK_USED, bytes }
+		ptr = pageTable[current_thread] + sizeof(Metadata);
+		*(SegMetadata *)ptr = segment;
+		//Set remaining page to free segment
+		char * nextPtr = ptr + (SegMetadata *)ptr->size + sizeof(SegMetadata);
+		int remainingPageSpace = PAGESIZE - ((nextPtr - 1) - pageTable[current_thread]);
+		segment = { BLOCK_FREE, remainingPageSpace };
+		*(SegMetadata *)nextPtr = segment;
+		
+		//Return pointer to user requested segment
+		return ptr + sizeof(SegMetadata);
 	}
-	
 	
 	
 	/* THIS IS IMPLEMENTATION FROM CS214
