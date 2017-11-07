62ba704e5e7ace8b1211cd93783db1d191ae2b2b
Alex <Alex>
2017-11-07 12:39:10 -0500

    Phase A complete/no more errors

--- my_malloc.c
+++ my_malloc.c
@@ -24,13 +24,24 @@ char ** pageTable;
 
 /** SMART MALLOC **/
 void* myallocate(int bytes, char * file, int line, int req){
+	// establish the base size of the kernel's space in memory
+	int kernelSize = sizeof(Metadata) 
+					 + (2 * MAX_NUM_THREADS * sizeof(pnode)) // pnodes allocation + buffer
+					 + (MAX_NUM_THREADS * sizeof(tcb)) // tcb allocation
+					 + (sizeof(pnode *) + sizeof(tcb **)) // MLPQ & tcbList
+					 + (MAX_NUM_THREADS * MEM) // stack allocations for child threads
+					 + ((MAX_NUM_THREADS + 2) * sizeof(char *)); //page table 
 	// "thread" var represents the calling thread's ID
 	int thread;
+	// "pagesize" var represents bound size of the current page (kernel vs user)
+	int pagesize;
 	if(req == THREADREQ) {
 		thread = current_thread;
+		pagesize = PAGESIZE;
 	}	
 	else if(req == LIBRARYREQ) {
 		thread = MAX_NUM_THREADS + 1;
+		pagesize = kernelSize;
 	}
 	else{
 		printf("Error! Invalid value for req: %d\n", req);
@@ -40,13 +51,6 @@ void* myallocate(int bytes, char * file, int line, int req){
 	// INITIALIZE KERNEL AND CREATE PAGE ABSTRACTION(FIRST MALLOC))
 	if(*myBlock == '\0'){
 		printf("Initializing kernel space in memory.\n");
-		// establish the base size of the kernel's space in memory
-		int kernelSize = sizeof(Metadata) 
-						 + (2 * MAX_NUM_THREADS * sizeof(pnode)) // pnodes allocation + buffer
-						 + (MAX_NUM_THREADS * sizeof(tcb)) // tcb allocation
-						 + (sizeof(pnode *) + sizeof(tcb **)) // MLPQ & tcbList
-						 + (MAX_NUM_THREADS * MEM) // stack allocations for child threads
-						 + ((MAX_NUM_THREADS + 1) * sizeof(char *)); //page table 
 		// the memory that would be "left over" after dividing the global block into pages,
 		// is lumped into the kernel's space
 		int remainingMem = (TOTALMEM - kernelSize ) % (PAGESIZE); 
@@ -58,7 +62,7 @@ void* myallocate(int bytes, char * file, int line, int req){
 		*(Metadata *)myBlock = data;
 		// pageTable is put in the "last" space in the kernel block... each cell stores a pointer, so
 		// pageTable is set to a casted pointer-array at this space in memory.
-		pageTable = (char **)((myBlock + kernelSize) - ((MAX_NUM_THREADS + 1) * sizeof(char *)));
+		pageTable = (char **)((myBlock + kernelSize) - ((MAX_NUM_THREADS + 2) * sizeof(char *)));
 		// MAX_NUM_THREADS + 1 is pages owned by scheduler/kernel, which means that its entry in
 		// pageTable should point to the Metadata for the kernel... which is at the beginning of myBlock.
 		pageTable[MAX_NUM_THREADS + 1] = myBlock;
@@ -105,19 +109,23 @@ void* myallocate(int bytes, char * file, int line, int req){
 	// Combine any consecutive, free segments in the current thread's page.
 	// At the same time, look and see if any free segments can store the user's request.
 	char * ptr = pageTable[thread] + sizeof(Metadata);
-	while (ptr < pageTable[thread] + PAGESIZE) {		
-		// TODO @all: does this logic account for multiple free, consecutive segments?
-		// are we iterating properly through the segments?
+	while (ptr < pageTable[thread] + pagesize) {		
 		printf("Checking for combinable segments in page for thread: %d\n", thread);
 		// If the current segment is free:
 		if (((SegMetadata *)ptr)->used == BLOCK_FREE) {
 			// If there is a following segment within the page:
-			if (ptr + ((SegMetadata *)ptr)->size + sizeof(SegMetadata) < pageTable[thread] + PAGESIZE) {
+			if ((ptr + ((SegMetadata *)ptr)->size + sizeof(SegMetadata)) < (pageTable[thread] + pagesize)) {
 				char * nextPtr = ptr + ((SegMetadata *)ptr)->size + sizeof(SegMetadata);
 					// If that following segment is free, combine the segments.
-					if (((SegMetadata *)nextPtr)->used == BLOCK_FREE) {		
-						printf("Combining segments in page for thread: %d\n", thread);
+					// Loop for further free segments
+					while (((SegMetadata *)nextPtr)->used == BLOCK_FREE) {		
 						((SegMetadata *)ptr)->size += ((SegMetadata *)nextPtr)->size + sizeof(SegMetadata); //Combine ptr & nextPtr segments
+						// If there is another segment in the page, increment, otherwise break
+						if ((nextPtr + ((SegMetadata *)nextPtr)->size + sizeof(SegMetadata)) < (pageTable[thread] + pagesize)) {
+							nextPtr = nextPtr + ((SegMetadata *)nextPtr)->size + sizeof(SegMetadata);
+						} else {
+							break;
+						}
 					}
 			}	
 			// If the current segment can hold the data, use it.
@@ -161,12 +169,12 @@ void mydeallocate(void * ptr, char * file, int line, int req){
 		return;
 	}
 	printf("Beginning mydeallocate for thread: %d\n", current_thread);
-	if((void *)myBlock > ptr || ptr > (void*)(myBlock + TOTALMEM) || ptr == NULL || ((*(Metadata *)(ptr-sizeof(Metadata))).used == BLOCK_FREE && (*(Metadata *)(ptr-sizeof(Metadata))).used != BLOCK_USED)){ 
+	if((void *)myBlock > ptr || ptr > (void*)(myBlock + TOTALMEM) || ptr == NULL || ((*(SegMetadata *)(ptr-sizeof(SegMetadata))).used == BLOCK_FREE && (*(SegMetadata *)(ptr-sizeof(SegMetadata))).used != BLOCK_USED)){ 
 		fprintf(stderr, "Pointer not dynamically located! - File: %s, Line: %d.\n", file, line);
 		return;
 	}
 	
-	if((*(Metadata *)(ptr - sizeof(Metadata))).used == BLOCK_FREE){
+	if((*(SegMetadata *)(ptr - sizeof(SegMetadata))).used == BLOCK_FREE){
 		fprintf(stderr, "Pointer already freed! - File: %s, Line: %d.\n", file, line);
 		return;
 	}
