2776fe05c91a2d62843cf8aa7b5dd32a998c25d7
BrunoJL4 <brunojameslucarelli@gmail.com>
2017-11-16 23:08:18 -0500

    Pushed benchmarks and test cases that got overwritten in Phase C push.

---
+++ Benchmark/baseTestCase.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "../my_pthread_t.h"
+
+int main(int argc, char **argv){
+
+	
+	
+	int * pointer = (int*)malloc(sizeof(int));
+	*pointer = 69; 
+
+	free(pointer);
+
+	printf("Finished freeing pointer!\n");
+	
+	return 0;
+}
